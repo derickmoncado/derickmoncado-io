0ff52f372c0a477297f202f6b40b4068b1f3a485
Derick Moncado <derickmoncado@gmail.com>
2024-03-16 15:21:51 -0700

    We'll come back to this

--- dist/assets/js/custom.js
+++ dist/assets/js/custom.js
@@ -108,7 +108,7 @@
 						<p>${snippetDesc}</p>
 					</div>
 					<div class="snippet__code">
-						<script src="https://gist.github.com/derickmoncado/${snippetId}.js"></script>
+						<script src="https://gist.github.com/derickmoncado/c7e44a12893d2712bc75659d0231d9d2.js"></script>
 					</div>
 				</li>
 			`;
