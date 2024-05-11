d283f758a63fb5f1bbca4d3774ccd0e3c8faba21
Derick Moncado <derickmoncado@gmail.com>
2024-05-11 15:37:22 -0700

    Couple of content tweaks

---
+++ src/assets/js/app.js
@@ -0,0 +1,7 @@
+// app.js
+import { greet } from "./greetings.js";
+
+const name = "John";
+const message = greet(name);
+
+console.log(message); // Outputs: Hello, John!
---
+++ src/assets/js/greeting.js
@@ -0,0 +1,8 @@
+// greetings.js
+const greeting = "Hello";
+
+const greet = (name) => {
+	return `${greeting}, ${name}!`;
+};
+
+export { greet };
